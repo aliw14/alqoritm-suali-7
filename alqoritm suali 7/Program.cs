4c72c56a7f729564df40c959a26021f7c1af8f4f
ali <ali@MacBook-Pro-Ali.local>
2023-01-03 02:32:08 +0400

    mesaj

---
+++ alqoritm suali 7/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace alqoritm_suali_7;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int count = 0;
+        int[] numbers = { 8, 9, 12, 4, 10 };
+        for (int i = 0; i < numbers.Length; i++)
+
+            if (numbers[i] % 7 == 1 || numbers[i] % 7 == 2 || numbers[i] % 7 == 5) 
+               
+
+            {
+                count++;
+
+            }
+         Console.WriteLine(count);
+
+
+
+    }
+}
+
